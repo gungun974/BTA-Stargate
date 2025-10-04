d5005ae3b94f4712e593bc7f3b3a9f05a29d0b39
Gungun974 <xfelix974@gmail.com>
2025-10-05 00:53:52 +0400

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# BTA Stargate
+
+This mod is my Stargate dream project.
+
+Discover and build Stargates to travel to new worlds.
+
+Uncover new technologies and ancient mysteries.
+
+---
+
+Requirements:
+
+- BTA (https://www.betterthanadventure.net)
+- Babric for BTA https://github.com/Turnip-Labs/babric-instance-repo/releases/tag/v7.3_04
+- HalpLibe (https://github.com/Turnip-Labs/bta-halplibe)
---
+++ src/main/java/gungun974/stargate/StargateMod.java
@@ -0,0 +1,37 @@
+package gungun974.stargate;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.util.GameStartEntrypoint;
+import turniplabs.halplibe.util.RecipeEntrypoint;
+
+
+public class StargateMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+    public static final String MOD_ID = "stargate";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Stargate initialized.");
+    }
+
+	@Override
+	public void onRecipesReady() {
+
+	}
+
+	@Override
+	public void initNamespaces() {
+
+	}
+
+	@Override
+	public void beforeGameStart() {
+
+	}
+
+	@Override
+	public void afterGameStart() {
+
+	}
+}
